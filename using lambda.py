41f257415b94567567ad56cd646c8f279a6e223e
asmaaSaid1 <130406417+asmaaSaid1@users.noreply.github.com>
2023-05-24 11:09:08 +0400

    Add files via upload

---
+++ using lambda.py
@@ -0,0 +1,23 @@
+
+"""
+c_to_f=lambda x:(x*9/5)+32
+print(c_to_f(30))"""
+
+"""
+mean=lambda a=4,b=6,c=7 :a+b+c/3
+print(mean())
+print(mean(8,5)) """
+
+"""
+#sorting list
+people=[{'name':'John','age':28},
+        {'name':'Mary','age':24},
+        {'name':'Bob','age':35},
+        {'name':'Alice','age':32}]
+
+
+sorted_list=sorted(people,key=lambda x:x['age'])
+
+print(sorted_list)
+"""
+
